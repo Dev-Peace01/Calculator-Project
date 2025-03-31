6ce5719640c976d22a19c1d775f8182cd5c5caeb
PeacefulCoder <140700967+Dev-Peace01@users.noreply.github.com>
2025-03-31 12:32:04 +0100

    this is my first commit

---
+++ asset/script/app.js
@@ -0,0 +1,57 @@
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// addBtn.addEventListener('click', add)
+// subtractBtn.addEventListener('click', subtract) 
+// multiplyBtn.addEventListener('click', multiply) 
+// divideBtn.addEventListener('click', divide)
+// divideBtn.addEventListener('click', clear)
+// divideBtn.addEventListener('click', delete)
+
+
+
+
+
+
+
+
+
+// onclick = display.value "0";
